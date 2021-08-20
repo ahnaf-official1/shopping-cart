e795722abd03a1377d9f8f0d25ac34d0c4c236b5
ahnaf-official1 <ahnafzareef2007@gmail.com>
2021-08-20 11:58:56 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,56 @@
+// Case number update
+function updateCaseNumber (product, price, isIncreasing){
+    const productInput = document.getElementById(product + '-number');
+    let productNumber = productInput.value;
+    if(isIncreasing == true) {
+        productNumber = parseInt(productNumber) + 1;
+    }  
+    else if(productNumber > 0 ){    
+        productNumber = parseInt(productNumber) - 1;
+    }
+    productInput.value = productNumber;   
+    // Update case total
+    const productTotal = document.getElementById(product + '-total');
+    productTotal.innerText = productNumber * price;
+ // calculate total
+    calculateTotal();
+}
+
+function getInputValue(product) {
+    const productInput = document.getElementById(product + '-number');
+    const productNumber = parseInt(productInput.value);
+    return productNumber;
+}
+
+function calculateTotal() {
+    const phoneTotal = getInputValue('phone') * 1219;
+    const caseTotal = getInputValue('case') * 59;
+    const subTotal = phoneTotal + caseTotal;
+    const tax = subTotal / 10;
+    const totalPrice = subTotal + tax;
+    // update on the html
+    document.getElementById('sub-total').innerText = subTotal;
+    document.getElementById('tax-total').innerText = tax;
+    document.getElementById('total-amount').innerText = totalPrice;
+}
+
+// Phone work
+document.getElementById('phone-plus').addEventListener('click',
+function(){
+    updateCaseNumber('phone', 1219, true);
+})
+document.getElementById('phone-minus').addEventListener('click',
+function(){
+    updateCaseNumber('phone', 1219, false);
+})
+
+// Case Work
+document.getElementById('case-plus').addEventListener('click',
+function(){
+    updateCaseNumber('case', 59, true);
+})
+
+document.getElementById('case-minus').addEventListener('click',
+function(){
+    updateCaseNumber('case', 59, false);
+})
